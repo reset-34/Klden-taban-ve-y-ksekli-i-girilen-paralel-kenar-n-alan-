daa9f702e177c702a7610dae53922656b34027cd
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:55:04 +0300

    Add project files.

---
+++ Klden taban ve yüksekliği girilen paralel kenarın alanı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klden_taban_ve_yüksekliği_girilen_paralel_kenarın_alanı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Klavyeden tabanı  ve yüksekliği girilen paralel kenarın alanını bulan C# programını yazınız.
+            Formül:
+            Taban*yükseklik
+             */
+
+            int taban, yukseklik, alan;
+
+            Console.WriteLine("Paralelkenarın tabanını giriniz");
+            taban = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Paralelkenarın yüksekliğini giriniz");
+            yukseklik = Int32.Parse(Console.ReadLine());
+
+            alan = taban * yukseklik;
+
+            Console.WriteLine("Paralel kenarın alanı =  " + alan);
+            Console.ReadLine();
+        }
+    }
+}
